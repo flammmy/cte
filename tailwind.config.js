ba019ce9db918ff601d81aea627f6f6ceffef1c7
abhishek <abhishp23@gmail.com>
2024-12-07 12:52:17 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./App.{js,jsx,ts,tsx}",
+    "./src/*/.{js,jsx,ts,tsx}",
+    "./components/*/.{js,jsx,ts,tsx}",
+    "./components/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
